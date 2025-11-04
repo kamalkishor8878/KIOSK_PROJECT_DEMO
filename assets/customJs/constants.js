64843332e8acd506b642b7648ae676891c5c5bff
kamalkishor8878 <119915172+kamalkishor8878@users.noreply.github.com>
2025-11-04 15:27:37 +0530

    Add thumbnail and change icons

--- assets/customJs/constants.js
+++ assets/customJs/constants.js
@@ -18,53 +18,64 @@ const categories = [
     image: "assets/images/lake.jpg",
   },
 ];
-const base = `file://${__dirname}/../../VIDEOS`;
-
+// Local videos list by category
 const videosData = {
   air: [
-    { title: "Sky Atmosphere Tutorial", file: `${base}/air/air.mp4` },
+    {
+      id: 1,
+      title: "Sky Atmosphere Tutorial",
+      thumbnail: "../VIDEOS/air/airThumbnail1.png",
+      file: "../VIDEOS/air/Educational puppet show on air quality.mov",
+    },
   ],
   water: [
-    { title: "Ocean & Rivers", file: `${base}/water/earthwatch_water_puppet_English.mp4` },
-    { title: "Ocean & Rivers", file: `${base}/water/earthwatch_water_puppet_Marathi.mp4` },
-    { title: "Ocean & Rivers", file: `${base}/water/earthwatch_water_puppet_Tamil.mp4` },
-    { title: "Ocean & Rivers", file: `${base}/water/earthwatch_water_puppet.mov` },
+    {
+      id: 2,
+      title: "जल ही जीवन है",
+      thumbnail: "../VIDEOS/water/Hindil_puppet_Portal.jpg",
+      file: "../VIDEOS/water/earthwatch_water_puppet.mov",
+    },
+    {
+      id: 3,
+      title: "Save Water",
+      thumbnail: "../VIDEOS/water/English_Puppet_Portal.jpg",
+      file: "../VIDEOS/water/earthwatch_water_puppet_English.mp4",
+    },
+    {
+      id: 4,
+      title: "पाणी हे जीवन आहे",
+      thumbnail: "../VIDEOS/water/Marathi_puppet_Portal.jpg",
+      file: "../VIDEOS/water/earthwatch_water_puppet_Marathi.mp4",
+    },
+    {
+      id: 5,
+      title: "நீர் தான் வாழ்க்கை",
+      thumbnail: "../VIDEOS/water/Tamil_puppet_Portal.jpg",
+      file: "../VIDEOS/water/earthwatch_water_puppet_Tamil.mp4",
+    },
   ],
   lake: [
-    { title: "Introduction to Lakes", file: `${base}/lake/lake1.mp4` },
+    {
+      id: 6,
+      title: "Educational Resource on Beautiful Wetlands and Biodiversity of Chennai",
+      about: `Promoting conservation of waterbodies and biodiversity through building knowledge and appreciation \n Explore | Learn | Inspire | Conserve`,
+      thumbnail: "../VIDEOS/lake/lakeThumbnail1.png",
+      file: "../VIDEOS/lake/lakeVideo1.mp4"
+    },
+
+    {
+      id: 7,
+      title: "Educational Resource on the Beautiful Lakes of Pimpri Chinchwad and Talegaon",
+      about: `Connecting people with lakes and biodiversity For today, tomorrow, and future generations`,
+      thumbnail: "../VIDEOS/lake/lakeThumbnail2.png",
+      file: "../VIDEOS/lake/lakeVideo2.mp4"
+    },
+    {
+      id: 8,
+      title: "Conservation of the Mangrove Ecosystem",
+      about: `Building knowledge, raising awareness, and strengthening community support for mangrove conservation`,
+      thumbnail: "../VIDEOS/lake/lakeThumbnail3.png",
+      file: "../VIDEOS/lake/lakeVideo3.mp4"
+    },
   ],
 };
-
-window.videosData = videosData;
-
-
-// // Local videos list by category
-// const videosData = {
-//   air: [
-//     {
-//       title: "Sky Atmosphere Tutorial",
-//       file: "assets/videos/air/airVideo.mov",
-//     },
-//   ],
-//   water: [
-//     {
-//       title: "Ocean & Rivers",
-//       file: "assets/videos/water/earthwatch_water_puppet_English.mp4",
-//     },
-//     {
-//       title: "Ocean & Rivers",
-//       file: "assets/videos/water/earthwatch_water_puppet_Marathi.mp4",
-//     },
-//     {
-//       title: "Ocean & Rivers",
-//       file: "assets/videos/water/earthwatch_water_puppet_Tamil.mp4",
-//     },
-//     {
-//       title: "Ocean & Rivers",
-//       file: "assets/videos/water/earthwatch_water_puppet.mov",
-//     },
-//   ],
-//   lake: [
-//     { title: "Introduction to Lakes", file: "assets/videos/lake/lake1.mp4" },
-//   ],
-// };
